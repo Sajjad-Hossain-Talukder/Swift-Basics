4d267f6ce3244cc4ca7e0d0b49e7b6d4f5950fed
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-12 16:09:14 +0600

    ClousureUpdated

--- ClousurePlayground.playground/Contents.swift
+++ ClousurePlayground.playground/Contents.swift
@@ -1,6 +1,10 @@
 
 
 
+
+// Function Inside another function as parameter
+
+/*
 func calculator ( n1 : Int , n2 : Int , operation : (Int,Int)->Int )->Int {
     return operation(n1,n2);
 }
@@ -20,8 +24,31 @@ func subtract ( n1:Int, n2:Int ) -> Int {
 
 
 calculator(n1 : 1, n2:34 ,operation: multiply )
-
 calculator(n1:100,n2:23, operation: add )
 calculator(n1:10,n2:2,operation: subtract  )
 
+*/
+
+// Clousure Starts Here
+// Syntex : { (parameterName: Type ) -> returnType in  return .... }
+
+
+func calculator ( n1 : Int , n2 : Int , operation : (Int,Int)->Int )->Int {
+    return operation(n1,n2);
+}
+
+
+calculator(n1: 4, n2: 5, operation:   {  (no1:Int,no2:Int) -> Int  in  return no1*no2  } )
+
+
+calculator(n1: 40, n2: 5, operation:   {  (no1,no2) in  return no1*no2  } )
+
+calculator(n1: 40, n2: 50, operation:   {  (no1,no2) in  no1*no2  } )
+
+// first parameter $0 and second parameter $1
+calculator(n1: 400, n2: 50, operation:   {  $0 * $1  } )
+
+
+// trailing clousure - if the last parameter is a clousure then, we mau come up with a TC
+calculator(n1: 400, n2: 50) {  $0 * $1  }
 
