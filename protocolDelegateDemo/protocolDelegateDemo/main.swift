60c90b91d6fe825735e529af7dfa337983cb0db4
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-12 11:46:46 +0600

    paramedicPortionAdded

--- protocolDelegateDemo/protocolDelegateDemo/main.swift
+++ protocolDelegateDemo/protocolDelegateDemo/main.swift
@@ -5,7 +5,45 @@
 //  Created by Xotech on 12/12/2023.
 //
 
-import Foundation
+protocol AdvanceLifeSupport {
+    func performCPR()
+}
 
-print("Hello, World!")
 
+
+class EmergencyCallHandler {
+    var delegate : AdvanceLifeSupport?
+    
+    func assessSituation() {
+        print("Tell me what happened ? ")
+    }
+    
+    func emergencyTreatment(){
+        delegate!.performCPR()
+    }
+}
+
+struct Paramedic : AdvanceLifeSupport {
+    let name : String
+    init(handler:EmergencyCallHandler, nm : String ){
+        name = nm
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("Paramedic \(name) Is here to help you. Keep Patience")
+    }
+    
+    func paraTask() {
+        print("Paramedic are responsible for ..... ")
+    }
+    
+}
+
+let emergencyWorkerSajjad = EmergencyCallHandler()
+let paramedicWorkerArman = Paramedic(handler: emergencyWorkerSajjad , nm: "Arman")
+let paramedicWorkerSakib = Paramedic(handler: emergencyWorkerSajjad , nm: "Sakib")
+
+
+emergencyWorkerSajjad.assessSituation()
+emergencyWorkerSajjad.emergencyTreatment()
