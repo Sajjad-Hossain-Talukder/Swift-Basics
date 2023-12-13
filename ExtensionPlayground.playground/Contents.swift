460215b917b3f71884c7fdc9dfb7b8cd06a422c0
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-13 14:09:46 +0600

    Double_DataType_Behaviour_Extended

--- ExtensionPlayground.playground/Contents.swift
+++ ExtensionPlayground.playground/Contents.swift
@@ -5,16 +5,50 @@ var pi = 3.14159
 pi.round()
 
 
-/*
 
+/*
+ 
  Extension Syntax:
  
  extension Datatype {
-    let n = self
-    statements - Add new functionality
+ 
+    statements - Add new functionality in function format
+ 
+    func newFunctionality () {
+        var n = self
+    }
+ 
  }
  
  */
+
+
+
+
+extension Double {
+    func round( to place : Int ) -> Double {
+        var n = self
+        let multiplier = pow( 10 , Double(place))
+        n *= multiplier
+        n.round()
+        n /= multiplier
+        return n
+    }
+}
+
+
+var piAgain = 3.14159
+
+var roundedToPlace = piAgain.round(to: 3)
+print(roundedToPlace)
+
+var roundedToPlace1 = piAgain.round(to: 1)
+print(roundedToPlace1)
+
+
+var roundedToPlace2 = piAgain.round(to: 4)
+print(roundedToPlace2)
+
 
 
 
