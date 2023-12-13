4b1c4415908c668b95f954ce15da88074c9ad973
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-13 13:57:46 +0600

    ExtensionAdded

---
+++ ExtensionPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var pi = 3.14159
+
+pi.round()
+
+
+/*
+
+ Extension Syntax:
+ 
+ extension Datatype {
+    let n = self
+    statements - Add new functionality
+ }
+ 
+ */
+
+
+
+
+
+
+
