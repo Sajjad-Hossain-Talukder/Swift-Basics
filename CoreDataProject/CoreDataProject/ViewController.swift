ad5a06832e6b04abb7ecca394e43ed0bc88b220c
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2024-01-09 15:34:07 +0600

    CoreDataProject Tested

---
+++ CoreDataProject/CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Xotech on 09/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
