7a0cbb42d66cafe5f4bab2c17fec326da041fbf6
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2024-01-18 14:39:12 +0600

    Custom Height Segue Added

--- SegueBasics/SegueBasics/ViewController.swift
+++ SegueBasics/SegueBasics/ViewController.swift
@@ -8,53 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var drawerHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if drawerHeightConstraint.constant > 0.0 {
-            self.drawerHeightConstraint.constant = 0
-        }
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    @IBAction func toggleDrawer(_ sender: Any) {
-        let newHeight: CGFloat
-        if drawerHeightConstraint.constant > 0.0 {
-            newHeight = 0
-            
-            print("height1 : ", view.layer.frame.height )
-        } else {
-            newHeight = view.layer.frame.height * 0.5034
-            print("height : ", view.layer.frame.height )
-        }
-            
-        UIView.animate(withDuration: 0.5 ) {
-            print(newHeight)
-            self.drawerHeightConstraint.constant = newHeight
-            self.view.layoutIfNeeded()
-        }
+        
     }
     
+    @IBAction func pressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "go", sender: self )
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           if segue.identifier == "goToTool" {
-               var secondVC = segue.destination as! SecondViewController
-               if let sheet = secondVC.sheetPresentationController{
-                   let multiplier =  0.2
-                   let height = self.view.frame.height
-                  
-                   let fraction = UISheetPresentationController.Detent.custom { context in
-                       100
-                    }
-                  
-                    sheet.detents = [fraction]
-               }
-               
-               
-              // present(secondVC, animated: true , completion: nil )
-           }
-       }
+        if segue.identifier == "go" {
+            var secondVC = segue.destination as! SecondViewController
+            if let sheet = secondVC.sheetPresentationController{
+                sheet.detents = [ UISheetPresentationController.Detent.custom { context in
+                    self.view.layer.frame.height * 0.509029
+                }]
+            }
+        }
+    }
+    
     
 }
 
